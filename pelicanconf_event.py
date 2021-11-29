3d0a0f3e007ae6a6f6b31f7b61eb0268b34611ec
Jose Miguel <josemiguellopez@glampinghub.com>
2021-11-29 20:46:54 +0100

    colors

--- pelicanconf_event.py
+++ pelicanconf_event.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
+import json
+
 EVENT_TITLE = "PyconES 2022"
 EVENT_SUBTITLE = "Granada"
 EVENT_DESCRIPTION = """
@@ -13,3 +15,17 @@ Con tu entrada podrás acceder los dos días de la conferencia completos.
 <br>
 Tambien puedes formar parte de nuestros patrocinadores y tener tu espacio dentro del evento.
 """
+
+EVENT_TRACKS = json.dumps([
+     { "id": 'a', "title": 'Room A' },
+     { "id": 'b', "title": 'Room B', "eventColor": 'green' },
+     { "id": 'c', "title": 'Room C', "eventColor": 'orange' },
+     { "id": 'd', "title": 'Room D', "eventColor": 'red' }
+])
+
+EVENT_TALKS = json.dumps([
+    { "id": '1', "resourceId": 'a', "start": '2020-09-07T09:00:00', "end": '2020-09-07T14:00:00', "title": 'event 2', "editable":False },
+    { "id": '2', "resourceId": 'b', "start": '2020-09-07T12:00:00', "end": '2020-09-08T06:00:00', "title": 'event 3', "editable":False },
+    { "id": '3', "resourceId": 'c', "start": '2020-09-07T07:30:00', "end": '2020-09-07T09:30:00', "title": 'event 4', "editable":False },
+    { "id": '4', "resourceId": 'd', "start": '2020-09-07T10:00:00', "end": '2020-09-07T15:00:00', "title": 'event 5', "editable":False }
+])
