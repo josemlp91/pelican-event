27072e0754f33be08586325475e93d33715fe1ad
Jose Miguel <josemiguellopez@glampinghub.com>
2021-11-30 09:23:09 +0100

    calendario

--- pelicanconf_event.py
+++ pelicanconf_event.py
@@ -6,13 +6,10 @@ import json
 EVENT_TITLE = "PyconES 2022"
 EVENT_SUBTITLE = "Granada"
 EVENT_DESCRIPTION = """
-Bienvenidos a la PyConES, la conferencia nacional sobre Python más importante de España.
-<br>
+Bienvenidos a la PyConES, la conferencia nacional sobre Python más importante de España.<br>
 Un evento de tres días que reunirá a los ponentes más interesantes, una agenda increíble y unas oportunidades de trabajo
-maravillosas.
-<br>
-Con tu entrada podrás acceder los dos días de la conferencia completos.
-<br>
+maravillosas.<br>
+Con tu entrada podrás acceder los dos días de la conferencia completos.<br>
 Tambien puedes formar parte de nuestros patrocinadores y tener tu espacio dentro del evento.
 """
 
@@ -23,9 +20,10 @@ EVENT_TRACKS = json.dumps([
      { "id": 'd', "title": 'Room D', "eventColor": 'red' }
 ])
 
+EVENT_START_DATE = "2020-09-07"
 EVENT_TALKS = json.dumps([
-    { "id": '1', "resourceId": 'a', "start": '2020-09-07T09:00:00', "end": '2020-09-07T14:00:00', "title": 'event 2', "editable":False },
-    { "id": '2', "resourceId": 'b', "start": '2020-09-07T12:00:00', "end": '2020-09-08T06:00:00', "title": 'event 3', "editable":False },
+    { "id": '1', "resourceId": 'a', "start": '2020-09-07T09:00:00', "end": '2020-09-07T14:00:00', "title": 'event 2', "editable":False, "description": "Ejemplo de descripción"},
+    { "id": '2', "resourceId": 'b', "start": '2020-09-08T09:00:00', "end": '2020-09-08T10:00:00', "title": 'event 3', "editable":False },
     { "id": '3', "resourceId": 'c', "start": '2020-09-07T07:30:00', "end": '2020-09-07T09:30:00', "title": 'event 4', "editable":False },
     { "id": '4', "resourceId": 'd', "start": '2020-09-07T10:00:00', "end": '2020-09-07T15:00:00', "title": 'event 5', "editable":False }
 ])
